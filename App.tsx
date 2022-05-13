9b5c9d2e1947a84f958e8ec8f7ec89435478536b
TGR <78166079+TGACPNV@users.noreply.github.com>
2022-05-13 08:27:53 +0200

    init project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Banner} from './app/components/Banner';
+import { AuthenticationForm} from './app/components/AuthenticationForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Banner />
+      <AuthenticationForm />
     </View>
   );
 }
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
   },
 });
