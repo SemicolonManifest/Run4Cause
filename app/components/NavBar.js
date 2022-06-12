8e8c34ad9a5ffae2f7afae25960d096fa69563a1
TGRCPNV <78166079+TGACPNV@users.noreply.github.com>
2022-06-12 18:30:22 +0200

    add a navbar with a drawer

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,18 +1,84 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Pressable } from 'react-native'
+import Button from '../components/Button'
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+import { theme } from '../core/theme'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
-export default function NavBar(){
+
+export default function NavBar(props) {
+    
+    const [isOpen, setIsOpen] = React.useState(false)
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+    const navigation = props.navigation
+    const navigateToDashboard = () => {
+        //navigation.navigate("LoginScreen")
+    }
+
+    const onSwipe = (gestureName, gestureState) => {
+        const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+        console.log(gestureName);
+      };
+
     return(
         <View style={styles.container}>
-        <Text style={styles.text}>NavBar</Text>
+            <View style={styles.navbar}>
+                <Pressable onPress={toggleDrawer}>
+                    <FontAwesomeIcon icon={faBars} size={30} color={theme.colors.text} />
+                </Pressable>
+            </View>
+            <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='left'
+                size={250}
+                styles={styles.drawer}
+            >
+                <Pressable onPress={toggleDrawer}>
+                    <FontAwesomeIcon icon={faXmark} size={30} color={theme.colors.text} onPress={toggleDrawer} />
+                </Pressable>
+                <Button mode="contained" onClick={navigateToDashboard} style={styles.navButton}>Dashboard</Button>
+                <Button mode="contained" onClick={toggleDrawer}>Close</Button>
+            </Drawer>
+            <GestureRecognizer onSwipe={onSwipe} style={styles.childrenView}>
+            
+            {props.children}
+            </GestureRecognizer>
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'dodgerblue',
+    navButton: {
+        borderRadius: 0,
+        width: '90%',
     },
-    text: {
+    navbar: {
+        paddingLeft: 10,
+        height: '5%',
+        justifyContent: 'center',
+    },
+    drawer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        backgroundColor: theme.colors.primary,
+        height: '100%',
+        width: '100%',
+        top: 0,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        
+    },
+    childrenView: {
+        flex1: 1,
+        height: '95%',
     }
 })
--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -4,33 +4,34 @@ import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
-import NavBar from '../components/NavBar'
 import User from '../models/User'
+import NavBar from '../components/NavBar'
 
 export default function Dashboard({ navigation }) {
   User.currentUser = JSON.parse(localStorage.getItem('currentUser'))
 
   return (
-    <Background>
-      <NavBar />
-      <Logo />
-      <Header>Let’s start</Header>
-      <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start editing this project.
-      </Paragraph>
-      <Button
-        mode="outlined"
-        onPress={() =>{
-          localStorage.removeItem("userToken");
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
+    <NavBar navigation={navigation}>
+      <Background>
+        <Logo />
+        <Header>Let’s start</Header>
+        <Paragraph>
+          Your amazing app starts here. Open you favorite code editor and start editing this project.
+        </Paragraph>
+        <Button
+          mode="outlined"
+          onPress={() =>{
+            localStorage.removeItem("userToken");
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'StartScreen' }],
+            })
+          }
         }
-      }
-      >
-        Logout
-      </Button>
-    </Background>
+        >
+          Logout
+        </Button>
+      </Background>
+    </NavBar>
   )
 }
